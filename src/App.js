761b425b91ea8903b7bdd157da0e4f111db56821
SAGOR <seakingmawmaw2020@gmail.com>
2021-06-09 17:49:11 +0600

    solving tolwing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import massenger from './image/massenger.png'
+
+const people = [
+  {
+    name: 'Shavaik',
+    email: 'shavik@example.com',
+    image:
+      'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?k=6&m=1179420343&s=612x612&w=0&h=y7GrwxrbixTWvJfaeiu55rWXMGYr6oP583uzJJ4-Kis=',
+  },
+  {
+    name: 'Rodson',
+    email: 'rodson.ramos@example.com',
+    image:
+      'https://img.freepik.com/free-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?size=626&ext=jpg'
+  },
+  {
+    name: 'Eat man',
+    email: 'eat.fox@example.com',
+    image:
+     'https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+  },
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Hamby',
+    email: 'hamby.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1586083702768-190ae093d34d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },  
+]
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-green-50'>
+      <div>
+      <div class="chat-notification">
+  <div class="chat-notification-logo-wrapper">
+    <img class="chat-notification-logo" src={massenger} alt="ChitChat Logo"/>
+  </div>
+  <div class="chat-notification-content">
+    <h4 class="chat-notification-title">ChitChat</h4>
+    <p class="chat-notification-message">your all friends are here</p>
+  </div>
+</div>
+
+      </div>
+      <div className='flex'>
+        <div className=" container mx-auto">
+        <ul className="divide-y divide-gray-200 container mx-auto">
+          {people.map((person) => (
+            <li key={person.email} className="py-4 flex">
+              <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+              <div className="ml-3">
+                <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                <p className="text-sm text-gray-500">{person.email}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        </div>
+       
+      </div>
     </div>
   );
 }
